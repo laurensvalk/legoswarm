c91cbd59ac3f15872061a6be55678b7ac2df3f49
Laurens Valk <laurensvalk@gmail.com>
2017-12-04 22:45:03 +0100

    separate class for hardware

--- miner/miner.py
+++ miner/miner.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
-import time
 import ev3dev.ev3 as ev3
 import sys
-from collections import defaultdict
+import time
 
 # Debug print
 def eprint(*args, **kwargs):
@@ -116,33 +114,3 @@ class RemoteControl:
 
     def IsPressed(self,button):
         return self.button_list.index(button) == self.remote.value()
-
-# Configure the devices
-remote = RemoteControl('in4')
-base = DriveBase(left='outB',right='outC',wheel_diameter=0.043,wheel_span = 0.12)
-picker = Picker('outA')
-
-
-
-actions = {
-    'NONE' : (0,0,None),
-    'LEFT_UP' : (0,40,None),
-    'RIGHT_UP' : (0,-40,None),
-    'BOTH_UP' : (6,0,None),
-    'BOTH_DOWN' : (-6,0,None),
-    'LEFT_DOWN' : (None, None, Picker.target_store),
-    'RIGHT_DOWN' : (None, None, Picker.target_open),
-    'BEACON' : (None, None, Picker.target_purge)    
-}
-actions = defaultdict(lambda: actions['NONE'], actions)
-
-while True:
-    speed_now, steering_now, target_now = actions[remote.Button()]
-
-    if target_now is not None:
-        picker.Goto(target_now)
-
-    if speed_now is not None and steering_now is not None:
-        base.DriveAndTurn(speed_now,steering_now)
-
-    time.sleep(0.1)
