558c8ddfe6636cd84ee1a47fdbbb9973a5e0b8f5
Laurens Valk <laurensvalk@gmail.com>
2017-12-24 12:08:11 +0100

    remove old python

--- behavior-sim-matlab/array_index.m
+++
@@ -1,7 +0,0 @@
-function [r_index, v_index, F_index] = array_index(i,p)
-    % Indexes for the current vehicle
-    r_index = (1:p.n)'+p.n*(i-1);
-    v_index = (1:p.m)'+p.m*(i-1);
-    F_index = (1:p.m)'+p.m*(i-1);
-end
-
