8ddf1db23c4aa8bc7be34df35333b33d0fe9ebef
Anton Vanhoucke <Anton@fabrique.nl>
2018-01-01 23:06:09 +0100

    legibility

--- position-server/test_udp_server.py
+++ position-server/test_udp_server.py
@@ -16,9 +16,20 @@ PORT = 50008
 # Bind the socket to the broadcast port
 server_address = ('255.255.255.255', PORT)
 
-robot_broadcast_data = {'markers': {1: [(0, 0), (4, 3)], 2: [(1919, 1079), (1919-4, 1079-3)], 3: [(960, 540), (960+4, 540-3)]}, 'balls': [], 
-'settings': {'sight_range': 300, 'dump_location': (20, 20), 'p_bot_midbase': (-4, -2), 'p_bot_gripper': (0, 5), 'field_height': 1080, 'field_width': 1920, 'cm_per_px': 0.1, 'speed_per_cm_spring_extension': 0.5 , 'turnrate_per_cm_spring_extension': 5}
-}
+robot_broadcast_data = {'markers': {1: [(0, 0), (4, 3)],
+                                    2: [(1919, 1079), (1919-4, 1079-3)],
+                                    3: [(960, 540), (960+4, 540-3)]},
+                        'balls': [],
+                        'settings': {'sight_range': 300,
+                                     'dump_location': (20, 20),
+                                     'p_bot_midbase': (-4, -2),
+                                     'p_bot_gripper': (0, 5),
+                                     'field_height': 1080,
+                                     'field_width': 1920,
+                                     'cm_per_px': 0.1,
+                                     'speed_per_cm_spring_extension': 0.5 ,
+                                     'turnrate_per_cm_spring_extension': 5}
+                        }
 
 
 if __name__ == '__main__':
