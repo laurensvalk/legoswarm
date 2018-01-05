36df7a08cd9c3f4797b5964109c1e403ab93a236
Anton Vanhoucke <Anton@fabrique.nl>
2018-01-05 16:57:27 +0100

    Clean up test UDP server

--- position-server/settings.py
+++ position-server/settings.py
@@ -1,31 +1,8 @@
-from numpy import array
-
-# robot_broadcast_data = {'markers': {#1: [(0, 0), (4, 3)],
-#                                     #2: [(1919, 1079), (1919-4, 1079-3)],
-#                                     #3: [(960, 540), (960+4, 540-3)]
-#                                     },
-#                         'localdata': {},
-#                         'balls': [],
-#                         'settings': {'sight_range': 300,
-#                                      'dump_location': (20, 20),
-#                                      'p_bot_midbase': (-4, -2),
-#                                      'p_bot_gripper': (0, 5),
-#                                      'field_height': 1080,
-#                                      'field_width': 1920,
-#                                      'cm_per_px': 0.1,
-#                                      'speed_per_cm_spring_extension': 0.1,
-#                                      'turnrate_per_cm_spring_extension': 0.5}
-#                         }
-
-robot_broadcast_data = {'markers': {3: [(1171, 851), (1211, 812)],
-                                    4: [(1547, 717), (1521, 673)],
-                                    1: [(929, 510), (895, 555)],
-                                    2: [(683, 380), (659, 332)]},
-                        'localdata': {
-                            'neighborgrippers': {1: {2: array([ 210.26,   19.82]), 3: array([ 99.6,  18.2])},
-                                                 2: {1: array([ 210.26,   19.82]), 3: array([ 110.66,    6.62])},
-                                                 3: {1: array([-40.56,  96.92]), 2: array([  32.54, -100.78])}}
-                            },
+robot_broadcast_data = {'markers': {#1: [(0, 0), (4, 3)],
+                                    #2: [(1919, 1079), (1919-4, 1079-3)],
+                                    #3: [(960, 540), (960+4, 540-3)]
+                                    },
+                        'localdata': {},
                         'balls': [],
                         'settings': {'sight_range': 300,
                                      'dump_location': (20, 20),
@@ -34,11 +11,9 @@ robot_broadcast_data = {'markers': {3: [(1171, 851), (1211, 812)],
                                      'field_height': 1080,
                                      'field_width': 1920,
                                      'cm_per_px': 0.1,
-                                     'speed_per_cm_spring_extension': 0.1,
-                                     'turnrate_per_cm_spring_extension': 0.5}}
-
-
-
+                                     'speed_per_cm_spring_extension': 0.2,
+                                     'turnrate_per_cm_spring_extension': 0.8}
+                        }
 
 
 SERVER_ADDR = ("255.255.255.255", 50008)
--- position-server/test_udp_server.py
+++ position-server/test_udp_server.py
@@ -3,6 +3,7 @@
 import socket
 from settings import robot_broadcast_data, SERVER_ADDR
 from parse_camera_data import preparse_robot_data
+from numpy import array
 import time
 try:
     import cPickle as pickle
@@ -13,19 +14,25 @@ except:
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-PORT = 50008
 
-# Bind the socket to the broadcast port
-SERVER_ADDR = ('255.255.255.255', PORT)
+robot_broadcast_data['markers'] = {3: [(1171, 851), (1211, 812)],
+                                    4: [(1547, 717), (1521, 673)],
+                                    1: [(929, 510), (895, 555)],
+                                    2: [(683, 380), (659, 332)]}
 
-robot_broadcast_data['markers'] = {1: [(0, 0), (4, 3)],
-                                    2: [(1919, 1079), (1919-4, 1079-3)],
-                                    3: [(960, 540), (960+4, 540-3)]}
+robot_broadcast_data['localdata'] = {'neighborgrippers':
+                                         {1: {2: array([ 210.26,   19.82]), 3: array([ 99.6,  18.2])},
+                                          2: {1: array([ 210.26,   19.82]), 3: array([ 110.66,    6.62])},
+                                          3: {1: array([-40.56,  96.92]), 2: array([  32.54, -100.78])}
+                                          }
+                                     }
 
 if __name__ == '__main__':
     while True:
         time.sleep(0.1)
-        robot_broadcast_data['localdata'] = preparse_robot_data(robot_broadcast_data['markers'], robot_broadcast_data['balls'], robot_broadcast_data['settings'])
+        robot_broadcast_data['localdata'] = preparse_robot_data(robot_broadcast_data['markers'],
+                                                                robot_broadcast_data['balls'],
+                                                                robot_broadcast_data['settings'])
         data = pickle.dumps(robot_broadcast_data)
         if data:
             try:
