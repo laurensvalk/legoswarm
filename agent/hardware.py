b8956988a1d25aaceb61226588c2cdcc7b3c6baa
Anton Vanhoucke <Anton@fabrique.nl>
2018-01-06 16:15:34 +0100

    Refactored the Motor Class to use more of the library.

--- agent/hardware.py
+++ agent/hardware.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import platform
+import ev3dev.auto as ev3
 
 # Check if code is running on the ev3
 def running_on_ev3():
@@ -8,52 +9,52 @@ def running_on_ev3():
     return 'ev3' in platform.platform()
 
 # Import ev3dev only if we're running on the EV3
-if running_on_ev3():
-    import ev3dev.ev3 as ev3
+# if running_on_ev3():
+#     import ev3dev.ev3 as ev3
 
 # Debug print
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-# TODO: This class (and whole module) needs better documentation
-class Motor:
 
-    max_speed_sp = 500 # degree per second
+class EZMotor(ev3.Motor):
+    """
+    Extending/subclassing the ev3dev motor class for ease of use.
+    """
+    # degree per second. Errors are raised somewhere above this number.
+    max_speed_sp = 600
 
     def __init__(self, port):
-        self.motor = ev3.Motor(port)
+        # self.motor = ev3.Motor(port)
+        ev3.Motor.__init__(self, port)
 
-    def SetSpeed(self, speed):
-        self.motor.speed_sp = self.limit(speed)
-        self.motor.run_forever()
+    def set_speed(self, speed):
+        self.speed_sp = self.limit(speed)
+        self.run_forever()
 
-    def GetSpeed(self):
-        return self.motor.speed
+    def get_speed(self):
+        return self.speed
     
-    def GetPosition(self):
-        return self.motor.position
+    def get_position(self):
+        return self.position
 
-    def Stop(self):
-        self.SetSpeed(0)    
+    # def Stop(self):
+    #     self.SetSpeed(0)
 
     def limit(self, speed):
         return max(min(self.max_speed_sp, speed), -self.max_speed_sp)       
 
-    def Goto(self, reference, speed, tolerance):
-        if not self.Running() and not (reference - tolerance <= self.GetPosition() <= reference + tolerance):
-            self.motor.position_sp = reference
-            self.motor.speed_sp = abs(self.limit(speed))
-            self.motor.run_to_abs_pos()
+    def go_to(self, reference, speed, tolerance):
+        if not self.is_running() and not (reference - tolerance <= self.position <= reference + tolerance):
+            self.position_sp = reference
+            self.speed_sp = abs(self.limit(speed))
+            self.run_to_abs_pos()
 
-    def Running(self):
-        return 'running' in self.motor.state
-
-    def WaitForCompletion(self):
-        while self.Running():
-            time.sleep(0.01)
+    def wait_for_completion(self):
+        self.wait_while('running')
 
     def __del__(self):
-        self.Stop()    
+        self.stop()
 
 class Picker:
     """Steer the picker mechanism to the desired target"""
@@ -78,7 +79,7 @@ class Picker:
 
         # If running on the EV3, perform a reset routine
         if self.running_on_ev3:
-            self.pickermotor = Motor(port)
+            self.pickermotor = EZMotor(port)
             self.__SetPickRate(-40)
             time.sleep(0.5)
 
@@ -89,11 +90,11 @@ class Picker:
 
     def __SetPickRate(self,rate):
         """Set the picker reference speed"""
-        self.pickermotor.SetSpeed(rate * self.motor_deg_per_picker_deg)
+        self.pickermotor.set_speed(rate * self.motor_deg_per_picker_deg)
 
     def __GetPickRate(self):
         """Get the current picker speed"""
-        return self.pickermotor.GetSpeed()/self.motor_deg_per_picker_deg
+        return self.pickermotor.get_speed() / self.motor_deg_per_picker_deg
 
     def __Stop(self):
         """Stop the picker motor"""
@@ -103,9 +104,9 @@ class Picker:
         """Steer Picker mechanism to desired target"""
         # If running on the EV3, steer picker to the desired target
         if self.running_on_ev3:
-            self.pickermotor.Goto(reference*self.motor_deg_per_picker_deg,          # Reference position
-                                  self.abs_speed*self.motor_deg_per_picker_deg,     # Speed to get there
-                                  abs(self.tolerance*self.motor_deg_per_picker_deg))# Allowed tolerance
+            self.pickermotor.go_to(reference * self.motor_deg_per_picker_deg,  # Reference position
+                                   self.abs_speed * self.motor_deg_per_picker_deg,  # Speed to get there
+                                   abs(self.tolerance*self.motor_deg_per_picker_deg))# Allowed tolerance
         else:
             print("Turning the gripper to: " + str(reference))
         
@@ -130,16 +131,16 @@ class DriveBase:
 
         # Initialize motors if running on the EV3
         if self.running_on_ev3:
-            self.left = Motor(left)
-            self.right = Motor(right)
+            self.left = EZMotor(left)
+            self.right = EZMotor(right)
 
     def DriveAndTurn(self, speed_cm_sec, turnrate_deg_sec):
         """Set speed of two motors to attain desired forward speed and turnrate"""
         nett_speed = speed_cm_sec / self.wheel_cm_sec_per_deg_s
         difference = turnrate_deg_sec * self.wheel_cm_sec_per_base_deg_sec / self.wheel_cm_sec_per_deg_s
         if self.running_on_ev3:
-            self.left.SetSpeed(nett_speed - difference)
-            self.right.SetSpeed(nett_speed + difference)
+            self.left.set_speed(nett_speed - difference)
+            self.right.set_speed(nett_speed + difference)
         else:
             print("Setting Drivebase to Speed cm/s: " + str(speed_cm_sec) + ", Turnrate deg/s: " + str(turnrate_deg_sec))
 
