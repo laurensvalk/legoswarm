52c58c6387f9ea92af299063314ef0953a56cc6d
Anton <antonvh@gmail.com>
2018-01-13 12:18:09 +0100

    clrea screen

--- agent/set_id.py
+++ agent/set_id.py
@@ -8,11 +8,13 @@ DEG_PER_ID = 20
 
 dial = ev3.Motor(ev3.OUTPUT_B)
 screen = ev3.Screen()
+btn = ev3.Button()
 
-while True:
+while not btn.enter:
     p = dial.position
     id = (p % (TOTAL_IDS * DEG_PER_ID))//TOTAL_IDS
-    screen.draw.text((10, 10), str(id), font=fonts.load('luBS24'))
+    screen.clear()
+    screen.draw.text((60, 60), str(id), font=fonts.load('luBS24'))
     screen.update()
     # print(id)
     time.sleep(0.3)
