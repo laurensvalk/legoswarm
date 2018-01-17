a32151832a41a2cd5ea2ef22a5bce3b0ca26f16d
Anton Vanhoucke <Anton@fabrique.nl>
2018-01-17 16:53:36 +0100

    detected playing field

--- position-server/position_server_cam.py
+++ position-server/position_server_cam.py
@@ -27,7 +27,7 @@ MIN_BALL_RADIUS_PX = 5
 MAX_BALL_RADIUS_PX = 16
 PLAYFIELD_COORDS = True
 ROBOT_FOOTPRINT_CENTER_OFFSET = np.array([60, -30])
-FILE = "" #"test_images/test.jpg" # 1920 x 1080 afbeelding. png mag ook.
+FILE = "test_images/1516199702.jpg" #"test_images/test.jpg" # 1920 x 1080 afbeelding. png mag ook.
 
 ### Initialize ###
 
@@ -123,10 +123,96 @@ class SocketThread(Thread):
 socket_server = SocketThread()
 socket_server.start()
 
+# Detect playing field
+while True:
+    if not FILE:
+        ok, img = cap.read()
+        if not ok:
+            continue    #and try again.
+    else:
+        img = cv2.imread(FILE)
+
+    img_grey = cv2.Canny(img, 80, 180)
+    img_grey = cv2.dilate(img_grey, np.ones((3,3)))
+    img_grey, contours, hierarchy = cv2.findContours(img_grey, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img = cv2.cvtColor(img_grey, cv2.COLOR_GRAY2BGR)
+    largest = 0
+    for c in contours:
+        approx = cv2.approxPolyDP(c, cv2.arcLength(c, True) * 0.02, True)
+        if len(approx) == 4:
+            area = cv2.contourArea(approx)
+            if area > largest:
+                playing_field = approx
+                largest = area
+                cv2.drawContours(img, [approx], -1, (255,0,255))
+    cv2.drawContours(img, [playing_field], -1, (0, 255, 0), thickness=8)
+    cv2.putText(img, "Press k if the playing field is detected", (150, 500), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 4)
+    cv2.imshow("cam", img)
+
+    # now that we have our screen contour, we need to determine
+    # the top-left, top-right, bottom-right, and bottom-left
+    # points so that we can later warp the image -- we'll start
+    # by reshaping our contour to be our finals and initializing
+    # our output rectangle in top-left, top-right, bottom-right,
+    # and bottom-left order
+    pts = playing_field.reshape(4, 2)
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # the top-left point has the smallest sum whereas the
+    # bottom-right has the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # compute the difference between the points -- the top-right
+    # will have the minumum difference and the bottom-left will
+    # have the maximum difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # multiply the rectangle by the original ratio
+    # rect *= ratio
+
+    # now that we have our rectangle of points, let's compute
+    # the width of our new image
+    (tl, tr, br, bl) = rect
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+
+    # ...and now for the height of our new image
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+
+    # take the maximum of the width and height values to reach
+    # our final dimensions
+    maxWidth = max(int(widthA), int(widthB))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # construct our destination points which will be used to
+    # map the screen to a top-down, "birds eye" view
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # calculate the perspective transform matrix and warp
+    # the perspective to grab the screen
+    M = cv2.getPerspectiveTransform(rect, dst)
+
+
+    # Wait for the 'q' key. Dont use ctrl-c !!!
+    keypress = cv2.waitKey(1) & 0xFF
+
+    if keypress == ord('k'):
+        break
+
+
 while True:
     # time.sleep(2)
     if not FILE:
-        ok, img_cam = cap.read()
+        ok, img = cap.read()
         if not ok:
             continue    #and try again.
     else:
@@ -134,9 +220,11 @@ while True:
     # crop
     # img = np.array(img[50:1000, 0:1850])
     # img = np.array(img_cam)
+    img = cv2.warpPerspective(img, M, (maxWidth, maxHeight))
     img_height, img_width = img.shape[:2]
 
     # convert to grayscale and adjust gamma curve
+    # img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_grey = adjust_curve(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
 
     # logging.debug("read greyscale image", t - time.time())
@@ -163,9 +251,6 @@ while True:
     for x in range(0, len(contours)):
         k = x
         c = 0
-
-        if len(contours[x]) == 4:
-            cv2.drawContours(img, [contours[x]], -1, (255,0,255))
 
         # Look for nested triangles
         while (hierarchy[0][k][2] != -1):
