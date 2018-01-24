8105948a48ffa490b4702e98dca0bf3fb97d148c
Anton Vanhoucke <Anton@fabrique.nl>
2018-01-24 21:08:43 +0100

    more refactoring

--- position-server/antoncv.py
+++ position-server/antoncv.py
@@ -50,13 +50,13 @@ def find_largest_n_side(img, sides=4):
     return img_grey, result
 
 
-def sorted_rect(input_rect):
+def sorted_rect(input_rect, dtype="float32"):
     # -- we'll start
     # by reshaping our contour to be our finals and initializing
     # our output rectangle in top-left, top-right, bottom-right,
     # and bottom-left order
     pts = input_rect.reshape(4, 2)
-    rect = np.zeros((4, 2), dtype="float32")
+    rect = np.zeros((4, 2), dtype=dtype)
     # the top-left point has the smallest sum whereas the
     # bottom-right has the largest sum
     s = pts.sum(axis=1)
@@ -72,7 +72,7 @@ def sorted_rect(input_rect):
     return rect
 
 
-def offset_convex_polygon(polygon, offset):
+def offset_convex_polygon(polygon, offset, dtype="float32", round_values=True):
     """
     Offsets a convex polygon by a certain amount of pixels
     :param polygon:
@@ -80,7 +80,7 @@ def offset_convex_polygon(polygon, offset):
     :return:
     """
     # First offset the playing field to include a bit of the black border
-    offset_polygon = np.zeros((4, 2), dtype="float32")
+    offset_polygon = np.zeros((4, 2), dtype=dtype)
     for i in range(-1, len(polygon)-1):
         to_prev = polygon[(i - 1)] - polygon[i]
         to_next = polygon[(i + 1)] - polygon[i]
@@ -90,9 +90,12 @@ def offset_convex_polygon(polygon, offset):
         w = unit_vector(to_prev) + unit_vector(to_next)
 
         # The new point p' is
-        offset_polygon[i] = polygon[i] + offset * w
+        p = polygon[i] + offset * w
+        if round_values:
+            p = np.around(p)
+        offset_polygon[i] = p
 
-    return offset_polygon
+    return offset_polygon.astype(dtype)
 
 
 def rect_from_image_size(width, height):
@@ -101,3 +104,51 @@ def rect_from_image_size(width, height):
         [width, 0],
         [width, height],
         [0, height]], dtype="float32")
+
+
+def find_nested_triangles(img, threshold=150, threshold_type="simple"):
+    triangles = []
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    if threshold_type == "simple":
+        # convert to grayscale and adjust gamma curve
+        img_grey = adjust_curve(img_grey)
+        # Simple adaptive mean thresholding
+        values, img_grey = cv2.threshold(img_grey, threshold, 255, cv2.THRESH_BINARY)
+
+    elif threshold_type == "otsu":
+        values, img_grey = cv2.threshold(img_grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    elif threshold_type == "adaptive":
+        img_grey = cv2.adaptiveThreshold(img_grey, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 17, 2)
+
+    elif threshold_type == "canny":
+        img_grey = cv2.Canny(img_grey, 50, 150)
+        img_grey = cv2.dilate(img_grey, np.ones((3, 3)))
+
+    # Find contours and tree
+    img_grey, contours, hierarchy = cv2.findContours(img_grey, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Find triangular contours with at least 2 children. These must be our markers!
+    for x in range(0, len(contours)):
+        k = x
+        c = 0
+
+        # Look for nested triangles
+        while (hierarchy[0][k][2] != -1):
+            # As long as k has children [2], find that child and look for children again.
+            # http://docs.opencv.org/3.1.0/d9/d8b/tutorial_py_contours_hierarchy.html
+            k = hierarchy[0][k][2]
+            c = c + 1
+
+        if hierarchy[0][k][2] != -1:
+            c = c + 1
+
+        if c == 2:
+            # To do: also check if it runs *exactly* 2 children deep. and not more.
+            # This marker has at least two children. Now let's check if it's a triangle.
+            approx = cv2.approxPolyDP(contours[x], cv2.arcLength(contours[x], True) * 0.05, True)
+            if len(approx) == 3:
+                triangles += [approx]
+
+    return img_grey, triangles
--- position-server/position_server_cam.py
+++ position-server/position_server_cam.py
@@ -11,9 +11,10 @@ import logging
 from threading import Thread
 from platform import platform
 
-from antoncv import adjust_curve, find_largest_n_side, sorted_rect, offset_convex_polygon, rect_from_image_size
+from antoncv import find_largest_n_side, sorted_rect, offset_convex_polygon, rect_from_image_size, \
+    find_nested_triangles
 from linalg import atan2_vec, vec_length
-from settings import settings, robot_settings, WIDTH, HEIGHT, FILE, SERVER_ADDR, THRESHOLD, PLAYING_FIELD_OFFSET
+from settings import settings, robot_settings, WIDTH, HEIGHT, FILE, SERVER_ADDR, PLAYING_FIELD_OFFSET
 from parse_camera_data import make_data_for_robots, bounding_box
 try:
     import cPickle as pickle
@@ -48,6 +49,7 @@ logging.basicConfig(#filename='position_server.log',     # To a file. Or not.
                     level=logging.INFO, )              # Log info, and warning
 
 ### UDP Server thread(s) ###
+
 
 class SocketThread(Thread):
     def __init__(self):
@@ -92,55 +94,55 @@ if __name__ == '__main__':
         else:
             img = cv2.imread(FILE)
 
-        img_grey, playing_field = find_largest_n_side(img, sides=4)
+        img_grey, largest_rect = find_largest_n_side(img, sides=4)
 
         # Optionally review edge-finding image
         # img = cv2.cvtColor(img_grey, cv2.COLOR_GRAY2BGR)
 
-        # Present the largest rectangle to the user
-        cv2.drawContours(img, [playing_field], -1, (0, 255, 0), thickness=8)
-        cv2.putText(img, "Press y if the playing field is detected, press n if not", (100, 500), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 4)
-
-        # now that we have our playing field contour, we need to determine
+        # Now that we have our playing field contour, we need to determine
         # the top-left, top-right, bottom-right, and bottom-left
-        # points so that we can later warp the image
-        rect = sorted_rect(playing_field)
-
+        # points so that we can later warp the image. So we sort the polygon points in this order
+        rect = sorted_rect(largest_rect)
         offset_rect = offset_convex_polygon(rect, PLAYING_FIELD_OFFSET)
 
+        # Present the found rectangles to the user
+        cv2.drawContours(img, [largest_rect], -1, (0, 255, 0), thickness=8)
         cv2.drawContours(img, [offset_rect.astype(int)], -1, (255, 0, 255), thickness=8)
-
-        # now that we have our rectangle of points, let's compute
-        # the width of our new image
-        (tl, tr, br, bl) = offset_rect
-        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-
-        # ...and now for the height of our new image
-        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-
-        # take the maximum of the width and height values to reach
-        # our final dimensions
-        maxWidth = max(int(widthA), int(widthB))
-        maxHeight = max(int(heightA), int(heightB))
-
-        # construct our destination points which will be used to
-        # map the screen to a top-down, "birds eye" view
-        dst = rect_from_image_size(maxWidth - 1, maxHeight - 1)
-
-        # calculate the perspective transform matrix and warp
-        # the perspective to grab the screen
-        M = cv2.getPerspectiveTransform(offset_rect, dst)
+        cv2.putText(img, "Press y if the playing field is detected, press n if not", (100, 500),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 4)
 
         cv2.imshow("cam", img)
         # Wait for the 'k' key. Dont use ctrl-c !!!
         keypress = cv2.waitKey(10) & 0xFF
 
         if keypress == ord('y'):
+            # now that we have our rectangle of points, let's compute
+            # the width of our new image
+            (tl, tr, br, bl) = offset_rect
+            widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+            widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+
+            # ...and now for the height of our new image
+            heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+            heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+
+            # take the maximum of the width and height values to reach
+            # our final dimensions
+            maxWidth = max(int(widthA), int(widthB))
+            maxHeight = max(int(heightA), int(heightB))
+
+            # construct our destination points which will be used to
+            # map the screen to a top-down, "birds eye" view
+            dst = rect_from_image_size(maxWidth - 1, maxHeight - 1)
+
+            # calculate the perspective transform matrix and warp
+            # the perspective to grab the screen
+            M = cv2.getPerspectiveTransform(offset_rect, dst)
+
             found_playing_field = True
             field_corners = offset_convex_polygon(dst, -PLAYING_FIELD_OFFSET)
             break
+
         elif keypress == ord('n'):
             found_playing_field = False
             field_corners = rect_from_image_size(WIDTH, HEIGHT)
@@ -159,135 +161,102 @@ if __name__ == '__main__':
         if found_playing_field:
             img = cv2.warpPerspective(img, M, (maxWidth, maxHeight))
 
-        # Get the size of the image
-        img_height, img_width = img.shape[:2]
+        robot_markers = {}
+        img_grey, triangles = find_nested_triangles(img)
 
-        # convert to grayscale and adjust gamma curve
-        img_grey = adjust_curve(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-
-        # Simple adaptive mean thresholding
-        values, img_grey = cv2.threshold(img_grey, THRESHOLD, 255, cv2.THRESH_BINARY)
-
-        # Alternative thresholding:
-        # values, img_grey = cv2.threshold(img_grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        # img_grey = cv2.adaptiveThreshold(img_grey, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 17, 2)
-        # img_grey = cv2.Canny(img_grey, 50, 150)
-        # img_grey = cv2.dilate(img_grey, np.ones((3, 3)))
-
-        # Find contours and tree
-        img_grey, contours, hierarchy = cv2.findContours(img_grey, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        # Uncomment to preview thresholded image
         # img = cv2.cvtColor(img_grey, cv2.COLOR_GRAY2BGR)
 
-        robot_markers = {}
-        # Find triangular contours with at least 2 children. These must be our markers!
-        for x in range(0, len(contours)):
-            k = x
-            c = 0
+        for triangle in triangles:
+            # Let it's corners be these vectors.
+            a = triangle[0][0]
+            b = triangle[1][0]
+            c = triangle[2][0]
 
-            # Look for nested triangles
-            while (hierarchy[0][k][2] != -1):
-                # As long as k has children [2], find that child and look for children again.
-                # http://docs.opencv.org/3.1.0/d9/d8b/tutorial_py_contours_hierarchy.html
-                k = hierarchy[0][k][2]
-                c = c + 1
+            # Now lets find the middle of the base of the triangle and the apex.
+            lengths = [vec_length(a - b), vec_length(b - c), vec_length(a - c)]
+            shortest = min(lengths)
+            shortest_idx = lengths.index(shortest)
+            if shortest_idx == 0:
+                midbase_marker = (a + b) / 2
+                apex_marker = c
+            elif shortest_idx == 1:
+                midbase_marker = (c + b) / 2
+                apex_marker = a
+            else:   # shortest == 'ac':
+                midbase_marker = (a + c) / 2
+                apex_marker = b
+            midbase_marker = midbase_marker.astype(int)
 
-            if hierarchy[0][k][2] != -1:
-                c = c + 1
+            # Find the direction in which the triangle is pointing
+            heading = atan2_vec(apex_marker - midbase_marker)
 
-            if c == 2:
-                # To do: also check if it runs *exactly* 2 children deep. and not more.
-                # This marker has at least two children. Now let's check if it's a triangle.
-                approx = cv2.approxPolyDP(contours[x], cv2.arcLength(contours[x], True)*0.05, True)
-                if len(approx) == 3:
-                    # We found a triangular object!
-                    # Let it's corners be these vectors.
-                    a = approx[0][0]
-                    b = approx[1][0]
-                    c = approx[2][0]
+            # Rotation matrix for reading code squares
+            c = np.cos(heading)
+            s = np.sin(heading)
+            R = np.array([[-s, -c], [-c, s]])
 
-                    # Now lets find the middle of the base of the triangle and the apex.
-                    lengths = [vec_length(a - b), vec_length(b - c), vec_length(a - c)]
-                    shortest = min(lengths)
-                    shortest_idx = lengths.index(shortest)
-                    if shortest_idx == 0:
-                        midbase_marker = (a + b) / 2
-                        apex_marker = c
-                    elif shortest_idx == 1:
-                        midbase_marker = (c + b) / 2
-                        apex_marker = a
-                    else:   # shortest == 'ac':
-                        midbase_marker = (a + c) / 2
-                        apex_marker = b
-                    midbase_marker = midbase_marker.astype(int)
+            # Calculate the relative position of the code dots with some linear algebra.
+            relative_code_positions = np.array([[0.375, 0.5],
+                                                [0.125, 0.5],
+                                                [-0.125, 0.5],
+                                                [-0.375, 0.5]])
 
-                    # Find the direction in which the triangle is pointing
-                    heading = atan2_vec(apex_marker - midbase_marker)
+            # Do a dot product of the relative positions with the center position,
+            # and offset this back to position of the robot to find matrix of absolute code pixel positions
+            locations = (midbase_marker + np.dot(relative_code_positions * shortest, R)).astype(int)
 
-                    # Rotation matrix for reading code squares
-                    c = np.cos(heading)
-                    s = np.sin(heading)
-                    R = np.array([[-s, -c], [-c, s]])
+            # Draw binary robot id marker positions
+            # for l in locations:
+            #     cv2.circle(img, tuple(l), 4, (0, 255, 0), -1)
 
-                    # Calculate the relative position of the code dots with some linear algebra.
-                    relative_code_positions = np.array([[0.375, 0.5],
-                                                        [0.125, 0.5],
-                                                        [-0.125, 0.5],
-                                                        [-0.375, 0.5]])
+            # Now check all code pixels and do a binary addition
+            robot_id = 0
+            for i in range(4):
+                try:
+                    p = img_grey[locations[i][1], locations[i][0]]
+                except:
+                    # The needed pixel is probably outside the image.
+                    robot_id = -1
+                    break
+                if not p:
+                    robot_id += 2 ** i
 
-                    # Do a dot product of the relative positions with the center position,
-                    # and offset this back to position of the robot to find matrix of absolute code pixel positions
-                    locations = (midbase_marker + np.dot(relative_code_positions * shortest, R)).astype(int)
+            # Draw the data
+            cv2.putText(img,
+                        u"{0:.2f} rad, code: {1}, x:{2}, y:{3}".format(heading,
+                                                                       robot_id,
+                                                                       midbase_marker[0],
+                                                                       midbase_marker[1]
+                                                                       ),
+                        tuple(midbase_marker),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 4)
 
-                    # Draw binary robot id marker positions
-                    # for l in locations:
-                    #     cv2.circle(img, tuple(l), 4, (0, 255, 0), -1)
+            # Draw the contour of our triangle
+            cv2.drawContours(img, [triangle], -1, (0, 255, 0))
 
-                    # Now check all code pixels and do a binary addition
-                    robot_id = 0
-                    for i in range(4):
-                        try:
-                            p = img_grey[locations[i][1], locations[i][0]]
-                        except:
-                            # The needed pixel is probably outside the image.
-                            robot_id = -1
-                            break
-                        if not p:
-                            robot_id += 2 ** i
+            # Black out the shape of the robot in our source image
+            bb = bounding_box(settings, midbase_marker, apex_marker)
+            cv2.drawContours(img, [bb], 0, (0, 0, 255), 2)
+            cv2.fillConvexPoly(img_grey, bb, 255)
 
-                    # Draw the data
-                    cv2.putText(img,
-                                u"{0:.2f} rad, code: {1}, x:{2}, y:{3}".format(heading,
-                                                                               robot_id,
-                                                                               midbase_marker[0],
-                                                                               midbase_marker[1]
-                                                                               ),
-                                tuple(midbase_marker),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 4)
-
-                    # Draw the contour of our triangle
-                    cv2.drawContours(img, [approx], -1, (0, 255, 0))
-
-                    # Black out the shape of the robot in our source image
-                    bb = bounding_box(settings, midbase_marker, apex_marker)
-                    cv2.drawContours(img, [bb], 0, (0, 0, 255), 2)
-                    cv2.fillConvexPoly(img_grey, bb, 255)
-
-                    # Save the data in our dictionary
-                    robot_markers[robot_id] = [(midbase_marker[0], midbase_marker[1]),  # Triangle Center with origin at bottom left
-                                               (apex_marker[0], apex_marker[1])]    # Triangle Top with origin at bottom left
+            # Save the data in our dictionary
+            robot_markers[robot_id] = [(midbase_marker[0], midbase_marker[1]),  # Triangle Center with origin at bottom left
+                                       (apex_marker[0], apex_marker[1])]    # Triangle Top with origin at bottom left
 
         # Found all robots, now let's detect balls.
         balls = []
 
-        # First erase the borders
         if found_playing_field:
+            # Get the size of the image
+            img_height, img_width = img.shape[:2]
+
+            # Erase the borders
             cv2.polylines(img_grey,
-                          np.array([[[0,0],[img_width,0],[img_width,img_height],[0,img_height]]]),
+                          np.array([rect_from_image_size(img_width, img_height)], dtype=int),
                           True,
                           255,
                           thickness=abs(PLAYING_FIELD_OFFSET)*2)
+
         # Now all robots & border are blacked out let's look for contours again.
         img_grey, contours, tree = cv2.findContours(img_grey, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         for c in contours:
