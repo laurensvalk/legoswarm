562ebeaf99addbe455045155f5a6e3f4efae275a
Laurens Valk <laurensvalk@gmail.com>
2018-01-27 16:10:06 +0100

    remove explicit calls to numpy in springs (dependency is still implicit for arithmetic)

--- agent/springs.py
+++ agent/springs.py
@@ -1,15 +1,39 @@
-import numpy as np
-
 class Spring():
     def __init__(self, characteristic):
         """Extract data from spring characteristic from settings"""
-        self.characteristic = np.array(characteristic)
-        self.extensions = self.characteristic[:,0]
-        self.forces     = self.characteristic[:,1]
+        self.forces = [force for (extension, force) in characteristic]
+        self.extensions = [extension for (extension, force) in characteristic]
+        self.npoints = len(self.extensions)
+        self.min_extension = self.extensions[0]
+        self.max_extension = self.extensions[-1]
 
     def get_force_magnitude(self, length):
         """Convert a given spring length to a force"""
-        return np.interp(length, self.extensions, self.forces)
+        # Custom implementation of interpolation
+
+        if length <= self.min_extension:
+            # if the extension is less than the minimum, take force corresponding to minimum length
+            force = self.forces[0]
+        elif length >= self.max_extension:
+            # if the extension is greater than the maximum, take force corresponding to maximum length
+            force = self.forces[-1]
+        else:
+            # Otherwise, our length is somewhere in between
+            for index in range(self.npoints):
+                # Loop over the extensions in pairs, looking at two at once
+                left = self.extensions[index-1]
+                right = self.extensions[index]
+
+                if left <= length <= right:
+                    # If the spring extension is in between the two selected ones, interpolate to find the force
+                    leftforce = self.forces[index-1]
+                    rightforce = self.forces[index]
+                    force = leftforce + (length-left)/(right-left)*(rightforce-leftforce)
+                    # We already have a force, so we can stop looking
+                    break
+        
+        # Return the result
+        return force
 
     def get_force_vector(self, spring_vector):
         """Convert a given spring vector to a force vector"""
@@ -18,7 +42,7 @@ class Spring():
         micron = 0.0001 
 
         # Obtain length and direction of spring 
-        length = np.linalg.norm(spring_vector)+micron
+        length = (spring_vector[0]**2+spring_vector[1]**2)**0.5+micron
         direction = spring_vector/length
         
         # Force magnitude
