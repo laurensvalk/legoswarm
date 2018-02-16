93957e7593f592d19526e1a65ca640b3e3b707be
Anton Vanhoucke <Anton@fabrique.nl>
2018-02-16 10:24:53 +0100

    Reenabled curve adjusting

--- position-server/antoncv.py
+++ position-server/antoncv.py
@@ -117,7 +117,7 @@ def find_nested_triangles(img, threshold=150, threshold_type="simple", depth=2):
 
     if threshold_type == "simple":
         # convert to grayscale and adjust gamma curve
-        # img_grey = adjust_curve(img_grey, factor=1.8)
+        img_grey = adjust_curve(img_grey, factor=1.4)
         # Simple adaptive mean thresholding
         values, img_grey = cv2.threshold(img_grey, threshold, 255, cv2.THRESH_BINARY)
 
